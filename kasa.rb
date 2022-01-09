55fc1580a8152b6a66bc4e20b151afa6fa0d3ec9
winebarrel <sugawara@winebarrel.jp>
2022-01-09 13:41:56 +0900

    update hash

--- kasa.rb
+++ kasa.rb
@@ -3,7 +3,7 @@ class Kasa < Formula
   homepage 'https://github.com/winebarrel/kasa'
   version '0.1.0'
   url "https://github.com/winebarrel/kasa/releases/download/v#{version}/kasa_#{version}_darwin_amd64.tar.gz"
-  sha256 '29eb5f70440e056dcb4c4da272edf9d089a6767f1490c778f8d08f64b0d77180'
+  sha256 '7f6ae9a273ee761ac8fcf7fabb5e70b73744e99540bfaa70f9ebe7c6340b60b0'
   license 'MIT'
 
   def install
