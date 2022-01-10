9c1f808c035be3eda2802ad2ba2ee2b6f9365c7b
winebarrel <sugawara@winebarrel.jp>
2022-01-10 15:07:09 +0900

    Bump up version

--- kasa.rb
+++ kasa.rb
@@ -1,9 +1,9 @@
 class Kasa < Formula
   desc 'CLI for esa.'
   homepage 'https://github.com/winebarrel/kasa'
-  version '0.2.0'
+  version '0.2.1'
   url "https://github.com/winebarrel/kasa/releases/download/v#{version}/kasa_#{version}_darwin_amd64.tar.gz"
-  sha256 '0b6eec28e1dcaac894d7765a91266dc9a47c4463e229255dfea669a0f455eb14'
+  sha256 '623a43aa4ba7d8e2bc8ea95f12721e1f8a5d665e7b9095c06e6f08a694b2868a'
   license 'MIT'
 
   def install
